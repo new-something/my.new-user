4a8ed0861a691f97472552e13066d7801ed608c7
ndgndg91 <ndgndg91@gmail.com>
2021-01-05 22:06:39 +0900

    <refactor> no need to parse jwt, just issue jwt

--- src/main/java/com/mynew/auth/global/JwtResolver.java
+++ src/main/java/com/mynew/auth/global/JwtResolver.java
@@ -1,8 +1,5 @@
 package com.mynew.auth.global;
 
-import com.mynew.auth.user.domain.User;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.AccessLevel;
@@ -10,13 +7,15 @@ import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 import java.time.ZonedDateTime;
-import java.util.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Log4j2
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class JwtResolver {
     private static final String KEY = "my-new-secret-key";
-    private static final long EXPIRATION_MINUTES = 21600; // 15일
+    private static final long EXPIRATION_MINUTES = 259200; // 180일
     private static final Map<String, Object> HEADERS;
 
     static {
@@ -35,6 +34,7 @@ public class JwtResolver {
         claims.put("id", id);
         claims.put("userName", userName);
         claims.put("email", email);
+        claims.put("roles", new String[]{"USER"});
 
         return Jwts.builder()
                 .setHeader(HEADERS)
@@ -43,44 +43,5 @@ public class JwtResolver {
                 .setExpiration(Date.from(ZonedDateTime.now().plusMinutes(EXPIRATION_MINUTES).toInstant()))
                 .signWith(SignatureAlgorithm.HS256, KEY.getBytes())
                 .compact();
-    }
-
-    public static boolean isExpired(final String jwt) {
-        Claims claims = parseJwtToClaims(jwt);
-        if (Objects.isNull(claims)) {
-            return false;
-        }
-
-        Date expiration = claims.getExpiration();
-        return expiration.before(new Date());
-    }
-
-    public static Claims parseJwtToClaims(final String jwt ){
-        try {
-
-            return Jwts.parser()
-                    .setSigningKey(KEY.getBytes())
-                    .parseClaimsJws(jwt)
-                    .getBody();
-        } catch (JwtException e) {
-            return null;
-        }
-    }
-
-    public static User parseJwtToUser(final String jwt){
-        Claims claims = parseJwtToClaims(jwt);
-        if (Objects.isNull(claims)) {
-            return User.EMPTY;
-        }
-
-        long id = claims.get("id", Long.class);
-        String nickName = claims.get("userName", String.class);
-        String email = claims.get("email", String.class);
-
-        return User.builder()
-                .userId(id)
-                .userName(nickName)
-                .email(email)
-                .build();
     }
 }
