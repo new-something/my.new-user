261bafd725080dcd2c963719cd1e1ebd12e59265
ndgndg91 <ndgndg91@gmail.com>
2021-01-11 21:07:53 +0900

    <test> aws cloud front api test

--- src/main/java/com/mynew/auth/global/controller/AwsHealthController.java
+++ src/main/java/com/mynew/auth/global/controller/AwsHealthController.java
@@ -1,8 +1,7 @@
 package com.mynew.auth.global.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AwsHealthController {
@@ -11,4 +10,30 @@ public class AwsHealthController {
     public ResponseEntity<String> ok(){
         return ResponseEntity.ok("aws health check in my new user api service");
     }
+
+    @GetMapping("/aws/{word}")
+    public ResponseEntity<String> getTest(@PathVariable String word){
+        return ResponseEntity.ok(String.format("Hello %s", word));
+    }
+
+    @PostMapping("/aws/{word}")
+    public ResponseEntity<String> postTest(@PathVariable String word) {
+        return ResponseEntity.ok(String.format("Hello %s", word));
+    }
+
+    @DeleteMapping("/aws/{word}")
+    public ResponseEntity<String> deleteTest(@PathVariable String word) {
+        return ResponseEntity.ok(String.format("Hello %s", word));
+    }
+
+    @PutMapping("/aws/{word}")
+    public ResponseEntity<String> putTest(@PathVariable String word) {
+        return ResponseEntity.ok(String.format("Hello %s", word));
+    }
+
+    @PatchMapping("/aws/{word}")
+    public ResponseEntity<String> patchTest(@PathVariable String word) {
+        return ResponseEntity.ok(String.format("Hello %s", word));
+    }
+
 }
