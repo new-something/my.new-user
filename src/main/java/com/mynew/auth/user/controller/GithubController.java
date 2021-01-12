73f032c52c1b2d835fb248192092b1272380eded
ndgndg91 <ndgndg91@gmail.com>
2021-01-12 22:38:18 +0900

    <feat> cookie max age 180 days

--- src/main/java/com/mynew/auth/user/controller/GithubController.java
+++ src/main/java/com/mynew/auth/user/controller/GithubController.java
@@ -34,6 +34,7 @@ public class GithubController {
         Cookie cookie = new Cookie("my-new-a", jwt);
         cookie.setDomain("my.new");
         cookie.setSecure(true);
+        cookie.setMaxAge(259200);
         response.addCookie(cookie);
         return ResponseEntity.ok(LoginCompleteResponse.ok(jwt));
     }
